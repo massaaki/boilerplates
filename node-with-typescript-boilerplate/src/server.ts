0819abc54c4207ca59657e07ad214a22949292d0
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-10-31 08:35:59 -0300

    lint adjusts

--- node-with-typescript-boilerplate/src/server.ts
+++ node-with-typescript-boilerplate/src/server.ts
@@ -2,11 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'hello world !'})
-});
-
+app.get('/', (request, response) => response.json({ message: 'hello world !' }));
 
 app.listen(3333, () => {
   console.log('🚀  Server started on port 3333.');
-})
+});
